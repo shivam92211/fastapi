eaf8e1fcd248ebfe02afea8e72e40c56c49d573c
Shivam Vishwakarma <shivam92211@gmail.com>
2023-10-23 17:52:59 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get('/')
-async def hello():
-    return {"Hello": "World"}
+@app.get("/extract-info")
+async def extract_info(request: Request):
+    referer = request.headers.get("Referer")
+    
+    user_agent = request.headers.get("User-Agent")
+    
+    
+    from user_agents import parse
 
-@app.get("/component/{component_id}") # path parameter
-async def get_components(component_id):
-    return {"component_id": component_id}
+    
+    user_agent_info = parse(user_agent)
+    browser = user_agent_info.browser.family
+    device = user_agent_info.device.family
 
-@app.get("/components/") # query parameter
-async def read_component(number: int , text: str):
-    return {"number": number, "text": text}
+    info = {
+        "Referer": referer,
+        "Browser": browser,
+        "Device": device,
+    }
+
+    return JSONResponse(content=info)
 
 
